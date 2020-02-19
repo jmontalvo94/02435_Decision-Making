d283283071518edbc4d884ed39f324f6ab71b1a1
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-02-20 00:14:34 +0100

    Update heat.jl

--- Exercises/Exercise 1/heat.jl
+++ Exercises/Exercise 1/heat.jl
@@ -7,8 +7,8 @@ using MathOptFormat
 #Sets
 scenarios = collect(1:3)
 periods = collect(1:24)
-chp_units = collect(1:1)
-heat_units = collect(1:2) #Unit 1 = GB, Unit 2= WCB
+units_chp = collect(1:1)
+units_heat = collect(1:2) #Unit 1 = GB, Unit 2= WCB
 
 #Electricity price forecast per hour and scenario
 e = [[245.41,275.07,225.8], [260.13,232.31,226.09], [271.21,237.84,223.64],
@@ -23,28 +23,53 @@ e = [[245.41,275.07,225.8], [260.13,232.31,226.09], [271.21,237.84,223.64],
 d = [[8.9,8.88,5.73],[8.7,9.08,7.24],[7.02,8.98,7.54],[7.02,9.28,7.54],[7.02,9.08,7.54],
 [6.03,9.18,7.14],[6.13,9.08,7.14],[5.83,9.28,7.44],[5.73,7.42,7.34],[6.03,7.12,7.14],
 [5.73,7.32,7.24],[7.14,8.56,7.54],[7.34,8.36,7.54],[7.34,8.36,7.14],[7.34,8.36,7.54],
-[7.44,8.36,7.24],[7.54,8.36,5.93],[7.14,10.58,5.93],[7.24,10.28,5.93],[7.44,10.48,6.29],
-[7.44,10.28,6.19],[7.54,10.18,6.39],[7.24,10.28,6.39],[7.34,10.18,6.59]]
+[7.44,8.36,7.24],[7.54,8.36,5.93],[7.14,10.58,5.93],[7.24,10.28,5.93],[7.44,10.48,6.29],[7.44,10.28,6.19],[7.54,10.18,6.39],[7.24,10.28,6.39],[7.34,10.18,6.59]]
 
 #Scenario probabilities
-pi = [0.5, 0.3125, 0.1875]
+π = [0.5, 0.3125, 0.1875]
 
 #Parameter CHP
-chp_qmax = 10
-chp_c = 600
-chp_phi = 1.1
+qmax_chp = 10
+c_chp = 600
+ϕ_chp = 1.1
 
 #Parameter Heat Units
-qmax = [10, 4]
-c = [420,250]
+qmax_heat = [10, 4]
+c_heat = [420,250]
 
 
 #Declare Gurobi model
 model_heat = Model(with_optimizer(Gurobi.Optimizer))
 
-#
-# INSERT YOUR MODEL HERE
-#
+@variable(model_heat, 0<=p_chp[i in units_chp, t in periods]) #Heat production for chp over period and scenario
+@variable(model_heat, 0<=p_heat[i in units_heat, t in periods, s in scenarios]) #Heat production for each unit, period and scenario
+
+#@variable(model_heat, 0<=s_in[t in periods, s in scenarios]) #Storage inflow per period and scenario
+#@variable(model_heat, 0<=s_out[t in periods, s in scenarios]) #Storage outflow per period and scenario
+#@variable(model_heat, 0<=s_level[t in periods, s in scenarios]) #Storage level per period and scenario
+#@variable(model_heat, 0<=q_miss[t in periods, s in scenarios]) #Missing heat
+
+#@objective(model_heat, Min, sum(c_chp[j]*p_chp[j,t] for j in units_chp for t in periods)-sum(pi[s]*e[t,s]*(1/ϕ_chp)*p_chp[j,t] for j in units_chp for t in periods for s in scenarios)+sum(pi[s]*c_heat[i,t]*p_heat[i,t,s] for i in units_heat for t in periods for s in scenarios))
+#@objective(model_heat, Min,sum(c_chp[j]*p_chp[j,t] - pi[s]*e[t,s]*(1/ϕ_chp)*p_chp[j,t] + pi[s]*c_heat[i,t]*p_heat[i,t,s] for i in units_heat for j in units_chp for t in periods for s in scenarios))
+
+function Σ(x)
+	return sum(x)
+end
+
+@objective(model_heat, Min, Σ( Σ(c_chp[j]*p_chp[j,t] - Σ(π[s]*e[t][s]*(1/ϕ_chp)*p_chp[j,t] for s in scenarios) for j in units_chp) + Σ(π[s]*c_heat[i]*p_heat[i,t,s] for i in units_heat for s in scenarios) for t in periods))
+
+#Max heat unit production
+@constraint(model_heat, max_heat_production[i in heat_units, t in periods], q_h[i,t] <= qmax[i])
+
+#Demand satisfaction
+@constraint(model_heat, demand_satisfaction[t in periods, s in scenarios],
+                sum(q_h[i,t] for i in heat_units) - s_in[t,s] + s_out[t,s]== d[t][s] - q_miss[t,s])
+
+#Storage balance
+#@constraint(model_heat, storage_balance[t in collect(2:24), s in scenarios], s_level[t,s]-s_level[t-1,s]-s_in[t,s]+s_out[t,s]==0)
+#@constraint(model_heat, storage_balance_init[s in scenarios], s_level[1,s]-s_in[1,s]+s_out[1,s]==0)
+#Storage capacity
+#@constraint(model_heat, storage_capacity[t in periods, s in scenarios], s_level[t,s] <= K)
 
 #Write model to an LP file for debugging
 lp_model = MathOptFormat.LP.Model()
@@ -56,11 +81,11 @@ optimize!(model_heat)
 
 
 if termination_status(model_heat) == MOI.OPTIMAL
-    
-	
-	#ADD ADDITIONAL OUTPUT HERE 
-	
-	
+
+
+	#ADD ADDITIONAL OUTPUT HERE
+
+
     @printf "\nObjective value: %0.3f\n\n" objective_value(model_heat)
 
 
